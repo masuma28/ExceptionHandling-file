3f0b7227912810015fff696e2c1cc13f93b5cd1f
masuma28 <aktermasuma2024@gmail.com>
2025-08-30 21:52:35 +0600

    Add files via upload

---
+++ File4.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+    public class File4 {
+        public static void main(String[] args) {
+            File inputFile = new File("input.txt");   // You should create input.txt manually
+            File outputFile = new File("File4.dat");
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+                 DataOutputStream writer = new DataOutputStream(new FileOutputStream(outputFile))) {
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    writer.writeUTF(line); // UTF-8 string
+                }
+                System.out.println("Lines written into Lab08_03.dat as UTF-8.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
